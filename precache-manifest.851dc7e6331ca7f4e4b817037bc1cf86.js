29de426e36bebf36a078719e64bc540a90b4afac
4k45hv3rm4 <4k45hr0ck5007@gmail.com>
2020-07-01 23:41:24 +0530

    Updates

---
+++ precache-manifest.851dc7e6331ca7f4e4b817037bc1cf86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb134e51d2188d73087c480a8c5b22f",
+    "url": "/akash/index.html"
+  },
+  {
+    "revision": "ed059028e7c666ae3c57",
+    "url": "/akash/static/css/main.892f6555.chunk.css"
+  },
+  {
+    "revision": "3eb3d944daf6594837f3",
+    "url": "/akash/static/js/2.b6a44bc3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/akash/static/js/2.b6a44bc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed059028e7c666ae3c57",
+    "url": "/akash/static/js/main.b8b78aa6.chunk.js"
+  },
+  {
+    "revision": "aeb999856da436d3f37c",
+    "url": "/akash/static/js/runtime-main.ead05e0f.js"
+  }
+]);
