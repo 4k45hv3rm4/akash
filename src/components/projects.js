457bee0b42820542e6f85faf09c29c85004d21de
4k45hv3rm4 <4k45hr0ck5007@gmail.com>
2020-07-01 23:27:32 +0530

    made changes

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,34 +1,42 @@
 import React from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {  faFile } from '@fortawesome/free-solid-svg-icons'
+import {  faFile, faQuestion, faSquare, faProjectDiagram, faSmile } from '@fortawesome/free-solid-svg-icons'
 const Project = ()=>{
     return (
         <div className="projectSection" id="project">
             <div className="Section">
                 <div className="element_card">
                     <p>
-                        <p>This is the project</p>
-                        <a href="">This is <FontAwesomeIcon icon={faFile}/> </a>
-
+                        <p>Quora Clone <FontAwesomeIcon icon={faSmile}/></p>
+                        <a href="https://quora-clone-web.herokuapp.com/home/">Site link <FontAwesomeIcon icon={faFile}/> </a>
+                        <br/>
+                        <a href="https://4k45hv3rm4/github/quora-clone">Github link <FontAwesomeIcon icon={faFile}/> </a>
+                        
                     </p>
                 </div>
                 <div className="element_card">
                     <p>
-                        <p>This is the project</p>
-                        <a href="">This is <FontAwesomeIcon icon={faFile}/></a>
+                        <p>Quora Clone <FontAwesomeIcon icon={faSmile}/></p>
+                        <a href="https://quora-clone-web.herokuapp.com/home/">Site link <FontAwesomeIcon icon={faFile}/> </a>
+                        <br/>
+                        <a href="https://4k45hv3rm4/github/quora-clone">Github link <FontAwesomeIcon icon={faFile}/> </a>
+                        </p>
+                </div>
+                <div className="element_card">
+                    <p>
+                        <p>Quora Clone <FontAwesomeIcon icon={faSmile}/></p>
+                        <a href="https://quora-clone-web.herokuapp.com/home/">Site link <FontAwesomeIcon icon={faFile}/> </a>
+                        <br/>
+                        <a href="https://4k45hv3rm4/github/quora-clone">Github link <FontAwesomeIcon icon={faFile}/> </a>
                     </p>
                 </div>
                 <div className="element_card">
                     <p>
-                         <p>This is the project</p>
-                        <a href="">This is <FontAwesomeIcon icon={faFile}/></a>
-                    </p>
-                </div>
-                <div className="element_card">
-                    <p>
-                        <p>This is the project</p>
-                        <a>This is <FontAwesomeIcon icon={faFile}/></a>
+                    <p>Quora Clone <FontAwesomeIcon icon={faSmile}/></p>
+                        <a href="https://quora-clone-web.herokuapp.com/home/">Site link <FontAwesomeIcon icon={faFile}/> </a>
+                        <br/>
+                        <a href="https://4k45hv3rm4/github/quora-clone">Github link <FontAwesomeIcon icon={faFile}/> </a>
                     </p>
                 </div>
             </div>
